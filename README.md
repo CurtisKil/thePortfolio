bca1f0df00391bb953d81d8108b2260b153ce722
Curtis <lucidck17@gmail.com>
2021-05-26 15:16:29 -0700

    Lots of stuff

---
+++ README.md
@@ -0,0 +1,1 @@
+little subscription of what you did and used to make this. HTML 5, features, animations, organization, etc.
--- script.js
+++ script.js
@@ -8,3 +8,42 @@ function fixNav() {
     nav.classList.remove("active");
   }
 }
+
+// Smooth Scroll
+const scroll = new SmoothScroll('.nav a[href*="#"]', '.logo a[href*="#"]', {
+  speed: 650,
+});
+
+// Expanding Cards
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
+
+// Contact Form Wave
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    // Split letters into array
+    .split("")
+
+    // Create array with each letter wrapped by a span
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+
+    // Turn it back into a string
+    .join("");
+});
