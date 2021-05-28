30391f3dd833874905c4ebc8cc3caca7f1594764
Curtis <lucidck17@gmail.com>
2021-05-28 12:06:17 -0700

    Typewriter Effect w/Constructor

--- script.js
+++ script.js
@@ -1,6 +1,71 @@
 const nav = document.querySelector(".nav");
 window.addEventListener("scroll", fixNav);
 
+const TypeWriter = function (txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = "";
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
+
+TypeWriter.prototype.type = function () {
+  // Current index of word
+  const current = this.wordIndex % this.words.length;
+  // Get full text of current word
+  const fullTxt = this.words[current];
+
+  // Check if deleting
+  if (this.isDeleting) {
+    // Remove character
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    // Add character
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  //  Insert txt into element
+  this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+  // Initial type speed
+  let typeSpeed = 300;
+
+  if (this.isDeleting) {
+    typeSpeed /= 2;
+  }
+
+  // If word is complete
+  if (!this.isDeleting && this.txt === fullTxt) {
+    // Make pause at end
+    typeSpeed = this.wait;
+    // Set delete to true
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    // Move to next word
+    this.wordIndex++;
+    // Pause before typing starts
+    typeSpeed = 500;
+  }
+
+  setTimeout(() => this.type(), typeSpeed);
+};
+
+// Init On DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  // Initialize Typewriter
+  new TypeWriter(txtElement, words, wait);
+}
+
+// Sticky Nav
 function fixNav() {
   if (window.scrollY > nav.offsetHeight + 150) {
     nav.classList.add("active");
